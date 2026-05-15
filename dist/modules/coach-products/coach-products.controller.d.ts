39f18eae23ce977017bbf2112d82dc86f48021ae
murarishettybhanu <murarishettybhanuteja@gmail.com>
2026-05-15 23:44:26 +0530

    chore: remove dist/ from git tracking, already in .gitignore

--- dist/modules/coach-products/coach-products.controller.d.ts
+++
@@ -1,8 +0,0 @@
-import { CoachProductsService } from './coach-products.service';
-export declare class CoachProductsController {
-    private readonly coachProductsService;
-    constructor(coachProductsService: CoachProductsService);
-    allot(data: any): Promise<import("./schemas/coach-product.schema").CoachProduct>;
-    findByCoach(coachId: string): Promise<import("./schemas/coach-product.schema").CoachProduct[]>;
-    remove(id: string): Promise<void>;
-}
